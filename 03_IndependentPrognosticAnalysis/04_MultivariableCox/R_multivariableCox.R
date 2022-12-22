98f6b77ce9860ce2e0122cff7ab75b3244f7b495
Zhe LIU <liuzhehayida@gmail.com>
2022-12-22 22:06:49 +0800

    Create R_multivariableCox.R

---
+++ 03_IndependentPrognosticAnalysis/04_MultivariableCox/R_multivariableCox.R
@@ -0,0 +1,58 @@
+setwd("D:/MyProjects/CTC/CTC_Prognosis_COAD/03_IndependentPrognosticAnalysis/04_MultivariableCox")
+rm(list=ls())
+
+library(survival)
+library(forestplot)
+
+clrs <- fpColors(box="red",line="darkblue", summary="royalblue")             #定义森林图颜色
+rt=read.csv("D:/MyProjects/CTC/CTC_Prognosis_COAD/03_IndependentPrognosticAnalysis/02_Time-dependent_ROC/indepInput.csv",header=T)
+head(rt)
+for(i in 1:(dim(rt)[1])){
+  rt$sample_name_new[i]<-paste(rt$sample_name[i],i,sep="_")
+}
+rownames(rt)<-rt$sample_name_new
+rt<-subset(rt, select = c("futime", "Status", "Age", "Gender", "Stage", "riskScore"))
+colnames(rt)<-c("futime", "fustat", "Age", "Gender", "Stage", "riskScore")
+head(rt)
+
+
+multiCox=coxph(Surv(futime, fustat) ~ ., data = rt)
+multiCoxSum=summary(multiCox)
+
+outTab=data.frame()
+outTab=cbind(
+  HR=multiCoxSum$conf.int[,"exp(coef)"],
+  HR.95L=multiCoxSum$conf.int[,"lower .95"],
+  HR.95H=multiCoxSum$conf.int[,"upper .95"],
+  pvalue=multiCoxSum$coefficients[,"Pr(>|z|)"])
+outTab=cbind(id=row.names(outTab),outTab)
+write.table(outTab,file="multiCox.xls",sep="\t",row.names=F,quote=F)
+
+#绘制森林图
+rt=read.table("multiCox.xls",header=T,sep="\t",row.names=1,check.names=F)
+data=as.matrix(rt)
+HR=data[,1:3]
+hr=sprintf("%.3f",HR[,"HR"])
+hrLow=sprintf("%.3f",HR[,"HR.95L"])
+hrHigh=sprintf("%.3f",HR[,"HR.95H"])
+pVal=data[,"pvalue"]
+pVal=ifelse(pVal<0.001, "<0.001", sprintf("%.3f", pVal))
+tabletext <- 
+  list(c(NA, rownames(HR)),
+       append("pvalue", pVal),
+       append("Hazard ratio",paste0(hr,"(",hrLow,"-",hrHigh,")")) )          #定义图片文字
+pdf(file="forest.pdf",onefile = FALSE,
+    width = 6,             #图片的宽度
+    height = 4,            #图片的高度
+)
+forestplot(tabletext, 
+           rbind(rep(NA, 3), HR),
+           col=clrs,
+           graphwidth=unit(50, "mm"),
+           xlog=T,
+           lwd.ci=2,
+           boxsize=0.3,
+           xlab="Hazard ratio"
+)
+dev.off()
+
